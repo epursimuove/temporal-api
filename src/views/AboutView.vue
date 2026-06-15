9bef6ba3bb186df7e55e898ab7db73195cc80036
Anders Gustafson <github@nemonisimors.com>
2026-06-15 17:26:44 +0200

    Update dependencies.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,4 +1,5 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <template>
   <h1>About</h1>
@@ -18,6 +19,8 @@
 
   <h2>Version history</h2>
 
+  <p>Version <var>1.1.5</var> was released in June 2026.</p>
+  
   <p>Version <var>1.1.4</var> was released in March 2026.</p>
 
   <p>Version <var>1.1.3</var> was released in November 2025.</p>
